d56f65658f01ce95b57c375b0b84f68b49460917
Priyanka Kotte <priyanka.kotte@ritwik.com>
2020-06-18 15:25:47 +0530

    updated uuid with timestamp

--- static/js/script.js
+++ static/js/script.js
@@ -9,7 +9,8 @@
 
 //initialization
 function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    //var uuidv4 = 
+	return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
       (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
   }
@@ -39,7 +40,7 @@ $(document).ready(function() {
 
     //if you want the bot to start the conversation
     // action_trigger();
-    user_id = uuidv4();
+    user_id = uuidv4()+"_"+Date.now();
     send("/home");
     var noteContent = '';
     //speech to text code starts here
